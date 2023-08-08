2ecba7594dae6156a992d755dc23e56e5a472c4d
DMcGehee17 <dmcgehee19@gmail.com>
2023-08-07 19:51:11 -0500

    June2023ClassSPA

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,4 @@
 import html from "html-literal";
-() => html`HTML template literal`;
-
-
 
 export default () => html `<footer>
     &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,4 @@
 import html from "html-literal";
-() => html`HTML template literal`;
-
-
 
 export default () => html `<header>
 <h1>Example SPA</h1>
